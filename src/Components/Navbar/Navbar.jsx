160b621f3c1f0b01b68a6d86580f7bd5214d3947
fcimah1 <eng.mohamedahmed2741991@gmail.com>
2023-12-12 07:06:10 +0200

    aa

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,22 +3,51 @@ import { Link, NavLink } from "react-router-dom";
 import { useRef, useState } from "react";
 import { useRecoilValue } from "recoil";
 import cartDetaials from "../../Atoms/Cart.atom";
+import $ from 'jquery';
 import './navbar.css'
 
 export default function Navbar() {
     const inpText = useRef()
     const [value, setValue] = useState("")
     const cart = useRecoilValue(cartDetaials)
+
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 400) {
+            $('nav .linkes').addClass('fixed');
+            $('nav .linkes .cart').removeClass('d-none');
+        } else {
+            $('nav .linkes').removeClass('fixed');
+            $('nav .linkes .cart').addClass('d-none');
+        }
+    });
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-primary bg-opacity-75">
-                <div className="container">
-                    <Link className="navbar-brand text-white fw-bold fs-3" to="/">SOUQ.COM</Link>
+            <nav className="navbar navbar-expand-lg">
+                <div className="container up-nav">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
+
+                    <Link className="navbar-brand text-white fw-bold fs-3" to="/">SOUQ.COM</Link>
+                    <div className="d-flex" role="search">
+                        <input ref={inpText} className="form-control me-2" onChange={() => {
+                            setValue(inpText.current.value)
+                        }} type="search" placeholder="Search" aria-label="Search" />
+                        <Link to={(value !== "") ? `search/${inpText.current.value}`
+                            : null} className="btn btn-secondary search" type="submit">Search</Link>
+                    </div>
+                    <div className="cart">
+                        <Link to="/cart" className="text-dark "
+                            onClick={console.log(cart)}>
+                            <AiOutlineShoppingCart />
+                            <p className="number-of-products">{cart.length}</p>
+                        </Link>
+                    </div>
+                </div>
+                <div className="container linkes">
                     <div className="collapse navbar-collapse  align-items-center" id="navbarSupportedContent">
-                        <ul className="navbar-nav m-auto mb-2">
+                        <ul className="navbar-nav  mb-2">
                             <li className="nav-item">
                                 <NavLink className="nav-link" to="/">Home</NavLink>
                             </li>
@@ -39,20 +68,13 @@ export default function Navbar() {
                             </li>
 
                         </ul>
-                        <div className="d-flex" role="search">
-                            <input ref={inpText} className="form-control me-2" onChange={() => {
-                                setValue(inpText.current.value)
-                            }} type="search" placeholder="Search" aria-label="Search" />
-                            <Link to={(value !== "") ? `search/${inpText.current.value}`
-                                : null} className="btn btn-secondary search" type="submit">Search</Link>
-                        </div>
-                        <div className="cart">
-                            <Link to="/cart" className="text-dark "
-                                onClick={console.log(cart)}>
-                                <AiOutlineShoppingCart />
-                                <p className="number-of-products">{cart.length}</p>
-                            </Link>
-                        </div>
+                    </div>
+                    <div className="cart">
+                        <Link to="/cart" className="text-dark "
+                            onClick={console.log(cart)}>
+                            <AiOutlineShoppingCart />
+                            <p className="number-of-products">{cart.length}</p>
+                        </Link>
                     </div>
                 </div>
             </nav>
